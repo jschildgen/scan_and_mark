8b472d32e73d05c1a012d730af44d4cadb000a49
SarahTreutinger <82441701+SarahTreutinger@users.noreply.github.com>
2024-11-12 16:19:16 +0100

    added maven shade

---
+++ src/main/java/org/example/Main_1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+//create new main -> new java doesn't like files with "extend application"
+public class Main_1 {
+    public static void main(String[] args) {
+        SAM.main(args);
+    }
+}
